314a80a863e83a51408f9dc618f15c155877080c
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-01-16 22:49:15 +0100

    add members app for login

---
+++ ASCC/members/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# Create your views here.
+
+def login_user(request):
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "success.html")
+        else:
+            messages.success(request, ("login fail"))
+            return render(request, "fail.html")
+    else:
+        return render(request, "authenticate/loginPage.html",{})
