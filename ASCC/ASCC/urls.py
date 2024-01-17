8c9b8adf0e6b3935e1587be68a0566e5b5ee3804
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-01-17 17:08:05 +0100

    finish login, registration

--- ASCC/ASCC/urls.py
+++ ASCC/ASCC/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("ASCCapp.urls")),
-    path("members/", include("members.urls")),
+    path("register/", v.register,name="register"),
+    path("",include("django.contrib.auth.urls"))
+    
     
 
 ]
--- ASCC/ASCCapp/views.py
+++ ASCC/ASCCapp/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request,"home.html")
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials.')
+
+    return render(request, 'registration/login.html')
+
+
